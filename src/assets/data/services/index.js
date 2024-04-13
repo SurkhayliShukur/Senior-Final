5aeac95b39e91f901ffa240ebafe5e9d13855bba
Shukur939 <shukursurkhayli000@gmail.com>
2024-04-13 15:34:54 +0400

    instanceAxios

---
+++ src/assets/data/services/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const instanceAxios = axios.create({
+    baseURL: "http://localhost:4000/"
+})
+
+instanceAxios.interceptors.request.use(
+    (config) => {
+        return config
+    },
+    (err) => {
+        return Promise.reject(err)
+    }
+)
