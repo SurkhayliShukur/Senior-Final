574899ce353b5378a4b00fadb4003cb6895abfde
Shukur939 <shukursurkhayli000@gmail.com>
2024-04-20 18:59:00 +0400

    some changes

--- src/components/Custom/Table/index.js
+++ src/components/Custom/Table/index.js
@@ -119,7 +119,11 @@ export const Table = ({ type, data, options, setOptions, handleCheckbox, basket,
                 fontSize: "14px",
                 backgroundColor: color,
               }}>
-              Add
+              <span className='text-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                </svg>
+              </span>
             </button>
           )
         }
