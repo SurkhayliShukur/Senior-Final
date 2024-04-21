7ebdde5bda45357bbc3b85ddb113a714ecd96a0f
Shukur939 <shukursurkhayli000@gmail.com>
2024-04-21 17:39:44 +0400

    layout

--- src/layouts/Card/index.js
+++ src/layouts/Card/index.js
@@ -1,11 +1,25 @@
 import React from "react"
+import { useFetchProduct } from "../../assets/data"
 
 export const Card = () => {
+    const { data } = useFetchProduct()
 
 
     return (
         <>
-            <div>Card</div>
+            {
+                data.map((product) => (
+                    <div class="card" key={product.id} style="width: 18rem;">
+                        <img src={product.image} class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h2>{product.title}</h2>
+                            <p class="card-text">{product.category}</p>
+                        </div>
+                    </div>
+                ))
+            }
+
+
         </>
     )
 
