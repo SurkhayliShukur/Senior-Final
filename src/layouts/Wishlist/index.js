a04591fd98770bf4edb0d0c0a4eb7ffb49178f03
Shukur939 <shukursurkhayli000@gmail.com>
2024-05-03 23:38:42 +0400

    wish page

--- src/layouts/Wishlist/index.js
+++ src/layouts/Wishlist/index.js
@@ -1,6 +1,12 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../Context/Theme';
 import { useDispatch, useSelector } from 'react-redux';
 import { getWishList } from '../../features/slices/wishSlice';
+
+
 export const Wishlist = () => {
+
+    const { fontColor } = useContext(ThemeContext)
 
     const wishList = useSelector(getWishList)
 
@@ -10,7 +16,9 @@ export const Wishlist = () => {
     return (
         <>
             <div>
-                Wishlist
+                <div className='my-3 mx-3 h2'>
+                    <span style={{ color: fontColor }}>Wishlist</span>
+                </div>
             </div>
         </>
     )
